0d79aec89988a7c98d93f250facbf2ebbc31cdeb
romakun <romak50@gmail.com>
2019-05-27 11:18:48 +0300

    Tests for new promoCodes

---
+++ src/test/java/shopGophotoweb/tests/promoCodes/ST7a_001.java
@@ -0,0 +1,95 @@
+package shopGophotoweb.tests.promoCodes;
+
+import org.testng.annotations.BeforeMethod;
+import shopGophotoweb.adminPages.*;
+import shopGophotoweb.pages.*;
+import webdriver.BaseTest;
+import webdriver.Browser;
+
+import static org.testng.Assert.assertTrue;
+
+
+public class ST7a_001 extends BaseTest {
+    @BeforeMethod
+    public void setPreconditions(){
+
+        logStep(1);
+        browser.navigate(Browser.getAdminPageUrl());
+        LoginPage loginPage=new LoginPage();
+        loginPage.login();
+
+        logStep(2);
+        AdminMainPage adminMainPage = new AdminMainPage();
+        adminMainPage.goToShop();
+
+        AdminProductsPage adminProductsPage=new AdminProductsPage();
+        Utilites.goToSidebarItem(Utilites.SidebarItems.Продажи);
+        Utilites.goToSidebarItem(Utilites.SidebarItems.оплаты);
+
+        logStep(3);
+        PaymetsMethodsPage paymetsMethodsPage=new PaymetsMethodsPage();
+        paymetsMethodsPage.unCheckAllMethods();
+        paymetsMethodsPage.checkPaymentMethodVisible(PaymetsMethodsPage.PaymentMethods.МОЙ_ВИД_ОПЛАТЫ_С_КОМИССИЕЙ_1);
+
+        logStep(4);
+        Utilites.goToSidebarItem(Utilites.SidebarItems.доставки);
+        DeliveryMethodsPage deliveryMethodsPage=new DeliveryMethodsPage();
+        deliveryMethodsPage.checkDeliveryMethodVisible(DeliveryMethodsPage.DeliveryMethods.Курьер);
+
+
+
+
+    }
+    @Override
+    public void runTest() throws InterruptedException {
+
+        logStep(1);
+        CatalogPage catalogPage=new CatalogPage();
+        catalogPage.goToProductPage("product1");
+        ProductPage product1Page=new ProductPage();
+        product1Page.addProductToCart();
+        Menu.goToCart();
+
+        logStep(2);
+        CartPage cartPage=new CartPage();
+        cartPage.fillInFields("test", "test", "tt@tt.tt");
+
+        logStep(3);
+
+        cartPage.selectDeliveryMethod(CartPage.DeliveryMethods.Курьер);
+        logger.info("Expected result: total price = 2 020 pуб.");
+        logger.info("Actual result: total price = "+cartPage.getTotalPrice());
+        assertEquals(cartPage.getTotalPrice(),"2 020 pуб.");
+
+        logStep(4);
+        cartPage.applyPromoCode("1a");
+        logger.info("Expected result: total price = 0 pуб.");
+        logger.info("Actual result: total price = "+cartPage.getTotalPrice());
+        assertEquals(cartPage.getTotalPrice(),"0 pуб.");
+
+        logStep(5);
+        cartPage.setProductCount("product1","5");
+        logger.info("Expected result: total price = 4 040 pуб.");
+        logger.info("Actual result: total price = "+cartPage.getTotalPrice());
+        assertEquals(cartPage.getTotalPrice(),"4 040 pуб.");
+
+        logStep(6);
+        cartPage.clickSubmit();
+        SuccessPage successPage=new SuccessPage();
+        assertTrue(successPage.isThanksForOrderMessageDisplayed());
+        String orderNumber=successPage.getOrderNumber();
+        logger.info("The order was completed");
+
+        logStep(7);
+        browser.navigate(Browser.getAdminPageUrl());
+        AdminMainPage adminMainPage = new AdminMainPage();
+        adminMainPage.goToShop();
+
+        logStep(8);
+        Utilites.goToMenuName("МАГАЗИН");
+        Utilites.goToSidebarItem(Utilites.SidebarItems.Продажи);
+        Utilites.goToSidebarItem(Utilites.SidebarItems.Заказы);
+        OrdersPage ordersPage=new OrdersPage();
+        assertEquals(ordersPage.getOrderTotalPrice(orderNumber),"4 040 pуб.");
+    }
+}
---
+++ src/test/java/shopGophotoweb/tests/promoCodes/ST7a_009.java
@@ -0,0 +1,75 @@
+package shopGophotoweb.tests.promoCodes;
+
+import shopGophotoweb.adminPages.AdminMainPage;
+import shopGophotoweb.adminPages.LoginPage;
+import shopGophotoweb.adminPages.OrdersPage;
+import shopGophotoweb.adminPages.Utilites;
+import shopGophotoweb.pages.*;
+import webdriver.BaseTest;
+import webdriver.Browser;
+
+import static org.testng.Assert.assertTrue;
+
+
+public class ST7a_009 extends BaseTest {
+
+    @Override
+    public void runTest() throws InterruptedException {
+
+        logStep(1);
+        CatalogPage catalogPage=new CatalogPage();
+        catalogPage.goToProductPage("product1");
+
+        ProductPage product1Page=new ProductPage();
+        product1Page.addProductToCart();
+        Menu.goToCart();
+
+        logStep(2);
+        CartPage cartPage=new CartPage();
+        cartPage.fillInFields("test", "test", "tt@tt.tt");
+
+        logStep(3);
+        cartPage.setProductCount("product1","5");
+        logger.info("Expected result: total price = 6 100.40 pуб.");
+        logger.info("Actual result: total price = "+cartPage.getTotalPrice());
+        assertEquals(cartPage.getTotalPrice(),"6 100.40 pуб.");
+
+
+        logStep(4);
+        cartPage.applyPromoCode("9a");
+        assertTrue(cartPage.isPromocodeErrorDisplayed());
+
+        logStep(5);
+        cartPage.setProductCount("product1","6");
+        logger.info("Expected result: total price = 7 120.50 pуб.");
+        logger.info("Actual result: total price = "+cartPage.getTotalPrice());
+        assertEquals(cartPage.getTotalPrice(),"7 120.50 pуб.");
+
+        logStep(6);
+        cartPage.applyPromoCode("9a");
+        logger.info("Expected result: total price = 5 151 pуб.");
+        logger.info("Actual result: total price = "+cartPage.getTotalPrice());
+        assertEquals(cartPage.getTotalPrice(),"5 151 pуб.");
+
+        logStep(7);
+        cartPage.clickSubmit();
+        SuccessPage successPage=new SuccessPage();
+        assertTrue(successPage.isThanksForOrderMessageDisplayed());
+        String orderNumber=successPage.getOrderNumber();
+        logger.info("The order was completed");
+
+        logStep(8);
+        browser.navigate(Browser.getAdminPageUrl());
+        LoginPage loginPage=new LoginPage();
+        loginPage.login();
+        AdminMainPage adminMainPage = new AdminMainPage();
+        adminMainPage.goToShop();
+
+        logStep(9);
+        Utilites.goToMenuName("МАГАЗИН");
+        Utilites.goToSidebarItem(Utilites.SidebarItems.Продажи);
+        Utilites.goToSidebarItem(Utilites.SidebarItems.Заказы);
+        OrdersPage ordersPage=new OrdersPage();
+        assertEquals(ordersPage.getOrderTotalPrice(orderNumber),"5 151 pуб.");
+    }
+}
