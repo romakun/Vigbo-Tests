24e6524f361faed3c391c5240f6e615a97cde6ac
romakun <romak50@gmail.com>
2019-05-27 11:30:30 +0300

    Изменение локаторов в корзине и на странице товара

--- src/test/java/shopGophotoweb/pages/ProductPage.java
+++ src/test/java/shopGophotoweb/pages/ProductPage.java
@@ -6,19 +6,19 @@ import webdriver.elements.*;
 
 public class ProductPage extends BaseForm {
     private Button btnAddToCart = new Button(By.id("skuadd"), "addToCartButton");
-    private  Button btnOutOfStock=new Button(By.xpath("//a[contains(text(),'Нет в наличии')]"),"Out of stock button");
-    private  Button btnPreOrder=new Button(By.xpath("//a[contains(text(),\"Оформить заказ\")]"),"PreOrder button");
-    private Label lblDiscount=new Label(By.xpath("//span[@class='product-price-discount']"),"Discount label");
-    private  Label lblOldPrice=new Label(By.xpath("//div[@class='price']/span[contains(@class,'product-price-old')]"),"Old price");
-    private Label lblPrice=new Label(By.xpath("//div[@class='price']/span[contains(@class,'product-price-min')]"),"Price label");
-    private  Label lblUnselectedItemError=new Label(By.xpath("//div[@class='error'][@data-prefix='Выберите']"),"Unselected item error");
+    private  Button btnOutOfStock = new Button(By.xpath("//a[contains(text(),'Нет в наличии')]"),"Out of stock button");
+    private  Button btnPreOrder = new Button(By.xpath("//a[contains(text(),\"Оформить заказ\")]"),"PreOrder button");
+    private Label lblDiscount = new Label(By.xpath("//span[@class='product-price-discount']"),"Discount label");
+    private  Label lblOldPrice = new Label(By.xpath("//div[@class='price']/span[contains(@class,'product-price-old')]"),"Old price");
+    private Label lblPrice= new Label(By.xpath("//div[@class='price']/span[contains(@class,'product-price-min')]"),"Price label");
+    private  Label lblUnselectedItemError = new Label(By.xpath("//div[@class='error'][@data-prefix='Выберите']"),"Unselected item error");
     //private  Label lblUnselectedItemError=new Label(By.xpath("//div[@class='error'][@data-prefix='Выберите']"),"Unselected item error");
     //private  ElemetsList listOptions=new ElemetsList(By.xpath("//select[@class=\"options\"]"),"List options");
-    private  String selectValueLocator="//select[@class='options']/option[.='%s'][not(@disabled)]";
-    private Button btnGoToCatalogBreadecrumbs=new Button(By.xpath("//nav[contains(@class, 'shop-bread-crumbs')]/a[contains(text(),'магазин')]"),"shop bread crumbs");
+    private  String selectValueLocator = "//select[@class='options']/option[.='%s'][not(@disabled)]";
+    private Button btnGoToCatalogBreadecrumbs = new Button(By.xpath("//nav[contains(@class, 'shop-bread-crumbs')]/a[contains(text(),'магазин')]"),"shop bread crumbs");
 
-    public ProductPage(){
-        super(By.xpath("//div[contains(@class,'page shop-product')]"),"Product Page");
+    public ProductPage() {
+        super(By.xpath("//div[contains(@class,'page shop-product')]"), "Product Page");
     }
 
 
